dd94e05715e9aa23d207119dc8f9219188b13ae8
Javier Ramos <javramrig@gmail.com>
2015-04-24 21:06:17 +0200

    Añadida plantilla del adaptador de la lista de historial de carreras.

---
+++ app/src/main/java/com/deltapps/runscore/HistorialBaseAdapter.java
@@ -0,0 +1,30 @@
+package com.deltapps.runscore;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Javier on 24/04/2015.
+ */
+public class HistorialBaseAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
