34fd7f794e86658870e1adeca5dc2325ab0167af
carlroma <carl.roma@gmail.com>
2015-05-13 22:47:22 +0200

    Añadido el funcionamiento de los botones ENTRAR y REGISTRAR. Al carajo los campos escondidos, se muestra todo y a chuparla.

--- app/src/main/java/com/deltapps/runscore/LoginActivity.java
+++ app/src/main/java/com/deltapps/runscore/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.deltapps.runscore;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.Parse;
 
@@ -45,5 +49,45 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void clickLogin (View view) {
+        EditText userEditText = (EditText) findViewById(R.id.user);
+        EditText passEditText = (EditText) findViewById(R.id.pass);
+        String user = userEditText.getText().toString();
+        String pass = passEditText.getText().toString();
+        MyParse myparse = new MyParse();
+        // Comprobamos si el login es correcto (true)
+        if(myparse.logIn(user, pass)) {
+            Intent intent = new Intent(this, HistorialActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),
+                    "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clickSignUp (View view) {
+        EditText userEditText = (EditText) findViewById(R.id.userS);
+        EditText emailEditText = (EditText) findViewById(R.id.emailS);
+        EditText passEditText = (EditText) findViewById(R.id.passS);
+        EditText pass2EditText = (EditText) findViewById(R.id.passS2);
+        String user = userEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String pass = passEditText.getText().toString();
+        String pass2 = pass2EditText.getText().toString();
+        MyParse myparse = new MyParse();
+        // Comprobamos si el registro es correcto (true)
+        if(myparse.signUp(user, email, pass, pass2)) {
+            Intent intent = new Intent(this, HistorialActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),
+                    "No se ha podido registrar", Toast.LENGTH_SHORT).show();
+        }
     }
 }
